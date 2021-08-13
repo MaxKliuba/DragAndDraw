f3f0059169e0113ed4d79647330d30988f7b388e
MaxKliuba <klyubamaks@gmail.com>
2021-08-13 15:15:31 +0300

    project added

---
+++ app/src/main/java/com/maxclub/android/draganddraw/DragAndDrawActivity.java
@@ -0,0 +1,11 @@
+package com.maxclub.android.draganddraw;
+
+import androidx.fragment.app.Fragment;
+
+public class DragAndDrawActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return DragAndDrawFragment.newInstance();
+    }
+}
---
+++ app/src/main/java/com/maxclub/android/draganddraw/DragAndDrawFragment.java
@@ -0,0 +1,30 @@
+package com.maxclub.android.draganddraw;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.jetbrains.annotations.NotNull;
+
+public class DragAndDrawFragment extends Fragment {
+
+    public static Fragment newInstance() {
+        return new DragAndDrawFragment();
+    }
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater,
+                             @Nullable @org.jetbrains.annotations.Nullable ViewGroup container,
+                             @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_drag_and_draw, container, false);
+
+        return v;
+    }
+}
